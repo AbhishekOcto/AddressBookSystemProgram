530329e3fc9efa82bdc2776917890a39c339d350
Abhishek <you@example.com>
2022-08-20 21:45:19 +0530

    [Abhishek] Added: Welcome message

---
+++ src/com/bridgelabz/addressBook/AddressBookMain.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.addressBook;
+
+public class AddressBookMain {
+	public static void welcome() {
+		System.out.println("*****Welcome to Address Book Program*****");
+	}
+
+	public static void main(String[] args) {
+		welcome();
+		// TODO Auto-generated method stub
+
+	}
+
+}
