d77009a514907e6d2e143a40f9cd9b65787e93b2
Abhishek <you@example.com>
2022-08-22 13:27:54 +0530

    [Abhishek] Added: UC4 Ability to delete a contact using first name of same

--- src/com/bridgelabz/addressBook/AddressBookMain.java
+++ src/com/bridgelabz/addressBook/AddressBookMain.java
@@ -18,7 +18,11 @@ public class AddressBookMain {
 		
 		do {
 			System.out.println("***ADDRESSBOOK SIMULATION***");
+<<<<<<< Updated upstream
 			System.out.println("\n1. ADD CONTACT \n2. DISPLAY CONTACT ");
+=======
+			System.out.println("\n1. ADD CONTACT \n2. DISPLAY CONTACT \n3. EDIT CONTACT \n4. DELETE A CONTACT \n5.EXIT ");
+>>>>>>> Stashed changes
 			System.out.println("Enter the Operation Number: ");
 			operationNo = sc.nextInt();
 			
@@ -28,12 +32,28 @@ public class AddressBookMain {
 				break;
 			case 2:
 				addressBook.displayContact();
+<<<<<<< Updated upstream
+=======
+				break;
+			case 3:
+				addressBook.editContact();
+				break;
+			case 4:
+				addressBook.deleteContact();
+				break;
+>>>>>>> Stashed changes
 			default:
 				System.out.println("Invalid entry");
 				break;
 			}
+<<<<<<< Updated upstream
 		} while (operationNo != 3);
     sc.close(); //closing the Scanner
+=======
+		} 
+		    while (operationNo != 5);
+            sc.close(); //closing the Scanner
+>>>>>>> Stashed changes
 	}
 	
 }
